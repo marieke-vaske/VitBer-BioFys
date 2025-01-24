6f1741158a0c7b69fccc365d67ffe97bfbd097ef
ibenkv <ibenvalvatne@gmail.com>
2025-01-24 17:26:20 +0100

    Sjekk ut den nye funksjonen min a gutta!

--- test.py
+++ test.py
@@ -8,3 +8,9 @@ while(True):
     i+=1
     if i==10:
         break
+
+def erBestFunksjon():
+    beste = input("Speil, speil på veggen der, hvem er best i landet her?")
+    print(f'{beste} er best i landet her')
+
+erBestFunksjon()
